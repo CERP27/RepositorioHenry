02f1fedff2d1dba0b8ad8a9f24caff5c5d874746
CERP <carloscer27r@gmail.com>
2023-03-14 16:42:58 -0300

    agregando clase ejemplo arreglos

---
+++ CarpetaHenry/arrays.js
@@ -0,0 +1,10 @@
+var listaDeCompras =[];
+
+listaDeCompras[3]="Tomates";
+
+listaDeCompras[1]="Lechugas";
+
+console.log(listaDeCompras.length);
+
+var elementoDelArray=listaDeCompras[1];
+//console.log(elementoDelArray);
